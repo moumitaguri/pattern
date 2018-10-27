baba7b97e852851c9a4fc41395ab742997f0d5d6
moumita das <44020214+moumitaguri@users.noreply.github.com>
2018-10-27 15:06:21 +0530

    added shellscript to run all tests

---
+++ run_all_tests.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+./run_test.sh pattern_rectangle.js input_rectangle expected_out_rectangle
+./run_test.sh pattern_triangle.js input_triangle expected_out_triangle
+./run_test.sh patternDiamond.js input_diamond expected_out_diamond
