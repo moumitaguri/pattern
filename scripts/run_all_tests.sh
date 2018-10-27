52b34024a56d7df205dce0d64ff3279f3e71b9c9
moumita das <44020214+moumitaguri@users.noreply.github.com>
2018-10-27 21:57:22 +0530

    Added run_all_test.sh

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
-./scripts/run_test.sh ./createRectangle.js ./app_test_data/input/input_rectangle ./app_test_data/output/expected_out_rectangle
-./scripts/run_test.sh ./createTriangle.js ./app_test_data/input/input_triangle ./app_test_data/output/expected_out_triangle
-./scripts/run_test.sh ./createDiamond.js ./app_test_data/input/input_diamond ./app_test_data/output/expected_out_diamond
+for testfile in ../test/*.js
+do
+  echo passing tests for $testfile;
+  node $testfile;
+done
