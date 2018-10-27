a597ce2855e5fa755feb3f79b44d103ad6f7d836
moumita das <44020214+moumitaguri@users.noreply.github.com>
2018-10-27 18:42:49 +0530

    Introduced scripts dir,consists of .sh files
    following .sh files are there
      run_test.sh
      run_all_tests.sh

--- run_all_tests.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-./run_test.sh createRectangle.js ./app_test_data/input/input_rectangle ./app_test_data/output/expected_out_rectangle
-./run_test.sh createTriangle.js ./app_test_data/input/input_triangle ./app_test_data/output/expected_out_triangle
-./run_test.sh createDiamond.js ./app_test_data/input/input_diamond ./app_test_data/output/expected_out_diamond
